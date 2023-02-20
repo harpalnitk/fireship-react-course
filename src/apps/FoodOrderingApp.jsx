80db7644d866a74d06bd9f59bfcb19d46cca779f
Harpal Singh <harpalnitk@gmail.com>
2023-02-20 20:46:07 +0530

    completed modal

--- src/apps/FoodOrderingApp.jsx
+++ src/apps/FoodOrderingApp.jsx
@@ -1,11 +1,24 @@
-import {useState, useEffect} from 'react';
-import classes from './FoodOrderingApp.module.css';
+// import {useState, useEffect} from 'react';
+// import classes from './FoodOrderingApp.module.css';
 import Header from '../components/FoodOrderingApp/Layout/Header';
 import Meals from '../components/FoodOrderingApp/Meals/Meals';
+import Cart from '../components/FoodOrderingApp/Cart/Cart';
+import { useState } from 'react';
 const FoodOrderingApp = (props) => {
+
+   const [cartIsShown, setCartIsShown]= useState(false);
+
+   const showCartHandler = ()=>{
+      setCartIsShown(true);
+   }
+
+   const hideCartHandler = ()=>{
+      setCartIsShown(false);
+   }
      return (
          <>
-         <Header/>
+         {cartIsShown && <Cart onClose={hideCartHandler}/>}
+         <Header onShowCart={showCartHandler}/>
          <main>
             <Meals/>
          </main>
